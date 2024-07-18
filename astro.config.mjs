8c479191d933741ad6650782528a554ac58765ef
ksk1015 <ksk.aizawa@gmail.com>
2024-07-18 16:54:51 +0900

    react, vue, svelte入れて、counterコンポーネントを作成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import netlify from "@astrojs/netlify";
+import react from "@astrojs/react";
+import vue from "@astrojs/vue";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify({
     edgeMiddleware: true
-  })
+  }),
+  integrations: [react(), vue(), svelte()]
 });
---
+++ src/components/SvelteCounter.svelte
@@ -0,0 +1,8 @@
+<script>
+  let count = 0;
+  function increment() {
+    count += 1;
+  }
+</script>
+
+<button on:click={increment}>{count}</button>
